8e0b9ff5941d1b7a82641680285d8ef2611709c5
icepeacful <icepeacful@163.com>
2026-01-03 20:03:17 +0800

    调整头部标签信息和网站图标

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="zh-CN">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
