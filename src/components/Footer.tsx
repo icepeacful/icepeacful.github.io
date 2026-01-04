07adb6b12a377b3438f2ee8f8ef7b55ee923f1cc
icepeacful <icepeacful@163.com>
2026-01-04 16:57:02 +0800

    新增网页布局组件：页脚

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+        <footer className="border-t py-10 text-center text-gray-400 text-sm">
+            © 2026-{new Date().getFullYear()} By 清雪榛子
+            <br />
+            春月间桐会 Built with Next.js & Tailwind.
+        </footer>
+    );
+}
