149ca10b0a4b22915595c4438d313fe487c36b3e
icepeacful <icepeacful@163.com>
2026-01-05 17:21:59 +0800

    调整主页显示，添加分页功能

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,16 @@ import Link from "next/link";
 import { ContentBlock } from "@/components/ContentBlock";
 import { Footer } from "@/components/Footer";
 import { NavBar } from "@/components/NavBar";
-import { getArticlesMetaData } from "@/lib/article";
+import {  getPaginatedArticles, getArticlesMetaData } from "@/lib/article";
+import { redirect } from "next/navigation";
+import { Pagination } from "@/components/Pagination";
 
 export default function Home() {
-  const articles = getArticlesMetaData();
+  // redirect("/page/1");
+  // return null;
+
+  // const articles = getArticlesMetaData();
+  const { articles, totalPages } = getPaginatedArticles(1);
 
   return (
     <div className="min-h-[calc(80vh)] bg-white text-gray-900 font-sans relative">
@@ -18,14 +24,7 @@ export default function Home() {
         </div>
       </main>
 
-      <div className="mt-16 text-center absolute bottom-10 w-full">
-        <Link 
-          href="/posts" 
-          className="inline-flex items-center text-sm font-bold border-b-2 border-black pb-1 hover:text-blue-600 hover:border-blue-600 transition-all"
-        >
-          查看所有文章 →
-        </Link>
-      </div>
+      <Pagination props={{ totalPages: totalPages, currentPage: 1 }} />
     </div>
   );
 }
