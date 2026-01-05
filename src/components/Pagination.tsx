55931ac4f7c7f2eff18f6e5fcd1f51029bf35659
icepeacful <icepeacful@163.com>
2026-01-05 18:12:05 +0800

    调整分页栏数字字体大小

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -8,7 +8,7 @@ const Pagination: React.FC<{ props: PaginationProps }> = ({ props }) => {
                 <Link
                     key={i + 1}
                     href={i == 0 ? `/` : `/page/${i + 1}`}
-                    className={`${i + 1 === props.currentPage ? "text-black" : "text-gray-400"} mr-1 ml-1 inline-flex items-center text-sm font-bold hover:text-blue-600 hover:border-blue-600 transition-all`}
+                    className={`${i + 1 === props.currentPage ? "text-black" : "text-gray-400"} mr-1 ml-1 inline-flex items-center text-xl font-bold hover:text-blue-600 hover:border-blue-600 transition-all`}
                 >
                     {i + 1}
                 </Link>
